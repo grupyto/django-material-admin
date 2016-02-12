d0d9898d79c62f2d9a31518e19fc5dac8e6fef19
Guilherme Carvalho <Guilherme Carvalho>
2016-02-12 11:48:58 -0300

    Adicionada a class Contacts para trabalhar com os inlines

--- demo/admin.py
+++ demo/admin.py
@@ -1,12 +1,15 @@
 # -*- encoding:utf8 -*-
 
 from django.contrib import admin
-from demo.models import Person, Group, Membership
+from demo.models import Person, Group, Membership, Contact
+
+class ContactAdmin(admin.StackedInline):
+    model = Contact
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'email']
     list_per_page = 1
-
+    inlines = [ContactAdmin, ]
 
 class MembershipAdmin(admin.ModelAdmin):
     date_hierarchy = 'date_joined'
--- demo/models.py
+++ demo/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class Contact(models.Model):
+    cell_phone = models.CharField(max_length=20)
+    twitter = models.CharField(max_length=20)
+    address = models.TextField(null=True, blank=True)
+    person = models.ForeignKey('Person')
 
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
