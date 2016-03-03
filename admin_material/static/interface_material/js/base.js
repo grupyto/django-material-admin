3b1fad34b197b5e4c28e51c463aca522dfbd3acb
guilhermecarvalho <guilherme.carvalho@palmas.to.gov.br>
2016-03-03 17:37:02 -0300

    Trabalhando no evento mouseover do icone menu drawer

--- admin_material/static/interface_material/js/base.js
+++ admin_material/static/interface_material/js/base.js
@@ -3,3 +3,11 @@ $(".gui-folder a").on("click", function () {
     $('.gui-controls-interno').hide(200);
     $('#ul-'+id).toggle(200);
 });
+
+
+// Evento para mostrar o menu caso o usuário passe o mouse sobre o item do menu
+$( "div[class='mdl-layout__drawer-button']" ).mouseover(function() {
+  /* Act on the event */
+  console.log('Over');
+  $( "div[class='manu-drawer']" ).addClass('is-visible');
+});
